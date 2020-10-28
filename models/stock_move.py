e897d0251e2618877174e821d274931bad3b8722
José Francisco Medina Aguayo <53542332+franciscomedinaag@users.noreply.github.com>
2020-10-28 12:18:59 -0600

    shopify_inventory_id

--- models/stock_move.py
+++ models/stock_move.py
@@ -17,7 +17,7 @@ class StockMoveLineInheritShopifyOdooInventorySalesSynchronisation(models.Model)
                     'sku': line.product_id.default_code,
                     'stock_qty': line.product_id.qty_available,
                     'price': line.product_id.list_price, 
-                    'inventory_item_id': line.product_id.inventory_item_id}
+                    'inventory_item_id': line.product_id.shopify_inventory_item_id}
             _logger.info("Loading data to webservice %s" % data)
             headers = {'Content-Type': 'application/json'}
             data_json = json.dumps({'params': data})
