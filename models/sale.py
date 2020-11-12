1f27a18e99dc28c83e639834d7f630452125b506
José Francisco Medina Aguayo <53542332+franciscomedinaag@users.noreply.github.com>
2020-11-12 14:15:43 -0600

    Update sale.py

--- models/sale.py
+++ models/sale.py
@@ -1,6 +1,6 @@
 from odoo import models, fields
 
-class SaleOrderInheritShopifyOdooInventorySalesSynchronisation(Models.models):
+class SaleOrderInheritShopifyOdooInventorySalesSynchronisation(models.Model):
   _inherit = 'sale.order'
 
   shopify_sale_order_id = fields.Char(string="Shopify Order ID") 
