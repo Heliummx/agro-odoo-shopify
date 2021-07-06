11f1833ffb68b837a0bdb9d2a671a96c95edb067
Heliummx <78097099+Heliummx@users.noreply.github.com>
2021-07-06 11:17:49 -0500

    discounts with taxes
    substract taxes from line price received

--- controllers/main.py
+++ controllers/main.py
@@ -211,7 +211,10 @@ class ShopifyOdooInventorySynchronisation(http.Controller):
             product_id = request.env['product.product'].sudo().search([('default_code', '=', line['sku'])], limit=1)
             
             if product_id:
-                product_dis = ( 1 - ( float(line.get('price')) / float(product_id.list_price) ) )  * 100  
+                line_price = float(line.get('price'))
+                for tax in product_id.taxes_id:
+                    line_price -= float(line.get('price')) * (1+(tax/100))
+                product_dis = ( 1 - line_price / float(product_id.list_price) ) )  * 100  
                 res.append((0, 0, {'product_id': product_id.id, 'product_uom_qty': line.get('quantity'), 'discount': product_dis }))
         return res
 
